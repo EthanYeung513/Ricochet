c2278b5321f8fac6f5d914077382ce0039947a22
EthanYeung513 <57107279+EthanYeung513@users.noreply.github.com>
2021-04-02 16:03:52 +0100

    Health bar implemented for enemy

---
+++ Assets/Scripts/EnemyHealthBar.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyHealthBar : MonoBehaviour
+{
+    [SerializeField] GameObject currentEnemy;
+    NormEnemy currentEnemyScript;
+    [SerializeField] GameObject player;
+    [SerializeField] Slider thisSlider;
+    [SerializeField] float currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisSlider = gameObject.GetComponent<Slider>();
+
+        player = GameObject.Find("Player"); //Set player variable to instance of player
+        currentEnemy = gameObject.transform.parent.gameObject.transform.parent.gameObject; //Set the enemy as the parent 
+        currentEnemyScript = currentEnemy.GetComponent<NormEnemy>(); //Get script of current Bullet
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    public void changeHealthBarValue()
+    {
+        currentHealth = currentEnemyScript.health; //Set health
+        thisSlider.value = currentHealth / 100;
+        Debug.Log(thisSlider.value);
+    }
+}
--- Assets/Scripts/NormEnemy.cs
+++ Assets/Scripts/NormEnemy.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class NormEnemy : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class NormEnemy : MonoBehaviour
 
     public int health = 100; //Set health to 100
 
+    public int bulletHit = 0;
+
+    public Slider currentSlider;
+    public EnemyHealthBar currentSliderScript;
+
     public GameObject player;
     public GameObject currentBullet; //Game object of current bullet that hit enemy
     public int currentBounces; //count of bounces of current bullet
@@ -22,7 +28,10 @@ public class NormEnemy : MonoBehaviour
     {
         player = GameObject.Find("Player"); //Set player variable to instance of player
         target = player.transform; //Set target to player transform
-        agent = GetComponent<NavMeshAgent>(); 
+        agent = GetComponent<NavMeshAgent>();
+        
+        currentSlider = gameObject.transform.GetChild(1).gameObject.GetComponent<Canvas>().transform.GetChild(0).gameObject.GetComponent<Slider>();
+        currentSliderScript = currentSlider.GetComponent<EnemyHealthBar>();
     }
 
     // Update is called once per frame
@@ -46,25 +55,37 @@ public class NormEnemy : MonoBehaviour
         }
         else if (other.CompareTag("Bullet")) //If it collided with bullet
         {
+            if (currentBullet == other.gameObject)
+            {
+                return;
+            }
             currentBullet = other.gameObject; //Set currentBullet
+            
             bulletBehaviour currentBulletScript = currentBullet.GetComponent<bulletBehaviour>(); //Get script of current Bullet
             currentBounces = currentBulletScript.bounceCount; //Get bounce count
-            calculateDamage(currentBounces); //Call function to calculate and deal damage
             Destroy(currentBullet); //Destroy this bullet
+            calculateDamage(currentBounces); //Call function to calculate and deal damage
+            Debug.Log("CALLED");
            
         }
     }
 
     void calculateDamage(int currentBounces)
     {
-        int damageToDeal = 50 + (currentBounces * 50);
+        int damageToDeal = 50;
         Debug.Log(damageToDeal);
+        bulletHit += 1;
+        Debug.Log(bulletHit);
 
         health -= damageToDeal;  //Take away from health
 
         if (health <= 0) 
         {
             Destroy(this.gameObject); //Health less than or = to 0, so destroy
+        }
+        else
+        {
+            currentSliderScript.changeHealthBarValue();
         }
 
     }
